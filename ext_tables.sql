81bc36c5e2716c60d35b6e348d3bc49902bed4ae
Christian Welzel <gawain@camlann.de>
2019-11-10 11:03:32 +0000

    [BUGFIX] #73, t3ver_id is removed from V 10
    Remove t3ver_id from table definitions and delete command.

--- ext_tables.sql
+++ ext_tables.sql
@@ -8,7 +8,6 @@ CREATE TABLE tx_gridelements_backend_layout (
 	crdate int(11) DEFAULT '0' NOT NULL,
 	cruser_id int(11) DEFAULT '0' NOT NULL,
 	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
 	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
 	t3ver_label varchar(30) DEFAULT '' NOT NULL,
 	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
@@ -29,8 +28,8 @@ CREATE TABLE tx_gridelements_backend_layout (
 	config text,
 	pi_flexform_ds mediumtext,
 	pi_flexform_ds_file text,
-	icon text,	
-	
+	icon text,
+
 	PRIMARY KEY (uid),
 	KEY parent (pid,deleted,hidden,sorting),
 	KEY t3ver_oid (t3ver_oid,t3ver_wsid)
