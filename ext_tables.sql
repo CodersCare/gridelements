0b6e664a9e436300e797d046b8cea456ba5a78c8
Jo Hasenau <cybercraft.jh@googlemail.com>
2020-01-17 10:53:47 +0000

    Merge branch '48-error-when-displaying-content-from-pages' into 'master'
    Resolve "Error when displaying content from Pages"
    
    Closes #48
    
    See merge request coderscare/gridelements!67
    
    (cherry picked from commit 06a942e08bf206302b97322b87a8a3e487acf24b)
    
    1b7761da [TASK] Unset field and add dummy DB field to fix problems with FIND_IN_SET
    72c42d29 Update ext_tables.sql
    fb44a916 [TASK] Unset field and add dummy DB field to fix problems with FIND_IN_SET
    6b5de727 Merge branch '48-error-when-displaying-content-from-pages' of...

--- ext_tables.sql
+++ ext_tables.sql
@@ -47,6 +47,7 @@ CREATE TABLE tt_content (
 	tx_gridelements_children int(11) DEFAULT '0' NOT NULL,
 	tx_gridelements_container int(11) DEFAULT '0' NOT NULL,
 	tx_gridelements_columns int(11) DEFAULT '0' NOT NULL,
+	gridelements_shortcut_page_order_by int(11) DEFAULT '0' NOT NULL,
 
 	KEY gridelements (tx_gridelements_container,tx_gridelements_columns)
 );
