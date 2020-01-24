640b41fc3157e74b989c7e264376184de05dba1b
Cybercraft <info@cybercraft.de>
2020-01-24 13:20:29 +0100

    [WIP] Replace Localization Controller to be capable of handling colPos -1
    ToDo: Properly deal with user selection of grid children via checkbox

---
+++ Configuration/Backend/AjaxRoutes.php
@@ -0,0 +1,20 @@
+<?php
+
+use GridElementsTeam\Gridelements\Backend;
+
+/**
+ * Definitions for routes provided by EXT:backend
+ * Contains all AJAX-based routes for entry points
+ *
+ * Currently the "access" property is only used so no token creation + validation is made
+ * but will be extended further.
+ */
+return [
+
+    // Get summary of records to localize
+    'records_localize_summary' => [
+        'path' => '/records/localize/summary',
+        'target' => Backend\LocalizationController::class . '::getRecordLocalizeSummary'
+    ]
+
+];
