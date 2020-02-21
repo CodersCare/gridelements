543bbe13b35f7a7f65ad6ba4a791127000760ef9
Jo Hasenau <cybercraft.jh@googlemail.com>
2020-02-21 12:07:18 +0000

    Revert "[WIP] Keep only items, that are not translated by parent containers anyeay"
    This reverts commit 7a89b4fa67db50addeaec32ee9efb153cf3492c7

--- Classes/Backend/LocalizationController.php
+++ Classes/Backend/LocalizationController.php
@@ -81,7 +81,6 @@ class LocalizationController
         $languageId = (int)$params['languageId'];
 
         $records = [];
-        $containers = [];
         $result = $this->localizationRepository->getRecordsToCopyDatabaseResult(
             $pageId,
             $destLanguageId,
@@ -94,31 +93,15 @@ class LocalizationController
             if (!$row || VersionState::cast($row['t3ver_state'])->equals(VersionState::DELETE_PLACEHOLDER)) {
                 continue;
             }
-            if ($row['CType'] === 'gridelements_pi1') {
-                $containers[$row['uid']] = true;
-            }
             $colPos = $row['colPos'];
-            $container = $row['tx_gridelements_container'];
-            $uid = $row['uid'];
-            if (!isset($records[$colPos]) && !isset($containers[$container])) {
+            if (!isset($records[$colPos])) {
                 $records[$colPos] = [];
             }
-            if (!isset($containers[$container])) {
-                $records[$colPos][] = [
-                    'icon'  => $this->iconFactory->getIconForRecord('tt_content', $row, Icon::SIZE_SMALL)->render(),
-                    'title' => $row[$GLOBALS['TCA']['tt_content']['ctrl']['label']],
-                    'uid'   => $uid
-                ];
-            }
-        }
-
-        // keep only those items, that are not translated by their parent container anyway
-        foreach ($records as $colPos => &$columnRecords) {
-            foreach ($columnRecords as $index => $record) {
-                if (isset($containers[$record['tx_gridelements_container']])) {
-                    unset($columnRecords[$index]);
-                }
-            }
+            $records[$colPos][] = [
+                'icon' => $this->iconFactory->getIconForRecord('tt_content', $row, Icon::SIZE_SMALL)->render(),
+                'title' => $row[$GLOBALS['TCA']['tt_content']['ctrl']['label']],
+                'uid' => $row['uid']
+            ];
         }
 
         return (new JsonResponse())->setPayload([
