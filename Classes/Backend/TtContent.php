37467456cff98553fdc38cc5385ece47ee92d1ae
Jo Hasenau <cybercraft.jh@googlemail.com>
2021-06-01 11:31:08 +0000

    Merge branch '180-illegal-offset-type-warning-with-gridelements-used-as-content-for-news-records' into 'master'
    Resolve "Illegal offset type warning with Gridelements used as content for news records"
    
    Closes #180
    
    See merge request coderscare/gridelements!106
    
    (cherry picked from commit 47a8d311fc28a3edab1517ee056289da7a275aa9)
    
    15f9010f Make sure $gridColumn is a string to avoid illegal offset types

--- Classes/Backend/TtContent.php
+++ Classes/Backend/TtContent.php
@@ -239,7 +239,7 @@ class TtContent
             }
             foreach ($params['items'] as $key => $container) {
                 $backendLayout = $containers[$container[1]]['tx_gridelements_backend_layout'];
-                $gridColumn = $params['row']['tx_gridelements_columns'];
+                $gridColumn = (string)$params['row']['tx_gridelements_columns'];
                 $allowed = $layoutSetups[$backendLayout]['allowed'][$gridColumn];
                 $disallowed = $layoutSetups[$backendLayout]['disallowed'][$gridColumn];
                 if ($container[1] > 0 && (!empty($allowed) || !empty($disallowed))) {
