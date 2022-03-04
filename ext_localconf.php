6f5a05061f5caa4beffe5efe205f1f4a7237667e
Jo Hasenau <info@cybercraft.de>
2022-03-04 10:04:04 +0100

    [BUGFIX] Make use of matching wizard classes for each TYPO3 version
    Reason: Gridelements 11 will be compatible to both CMS 10 and 11

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,12 +4,19 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1488914437] = [
-    'nodeName' => 'belayoutwizard',
-    'priority' => 50,
-    'class'    => \GridElementsTeam\Gridelements\Wizard\GridelementsBackendLayoutWizardElement::class,
-];
-
+if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 11000000) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1488914437] = [
+        'nodeName' => 'belayoutwizard',
+        'priority' => 50,
+        'class'    => \GridElementsTeam\Gridelements\Wizard\GridelementsBackendLayoutWizardElement::class,
+    ];
+} else {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1488914437] = [
+        'nodeName' => 'belayoutwizard',
+        'priority' => 50,
+        'class'    => \GridElementsTeam\Gridelements\Wizard\GridelementsBackendLayoutWizardElement10::class,
+    ];
+}
 // XCLASS
 if ($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['gridelements']['nestingInListModule']) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Recordlist\RecordList\DatabaseRecordList::class] = ['className' => \GridElementsTeam\Gridelements\Xclass\DatabaseRecordList::class];
