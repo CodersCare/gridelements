CodersCare/gridelements

commit e7e885ef28716766c1eb5916f76e359244c97362

Resolve "PHP (8.1) Warnings in Backend page module"

Classes/Hooks/DatabaseRecordList.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GridElementsTeam\Gridelements\Hooks;
 
 /***************************************************************
@@ -41,12 +43,12 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
     /**
      * @var Iconfactory
      */
-    protected $iconFactory;
+    protected IconFactory $iconFactory;
 
     /**
      * @var LanguageService
      */
-    protected $languageService;
+    protected LanguageService $languageService;
 
     /**
      * DatabaseRecordList constructor.
@@ -82,7 +84,7 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
      *
      * @return array the modified clip-icons
      */
-    public function makeClip($table, $row, $cells, &$parentObject)
+    public function makeClip($table, $row, $cells, &$parentObject): array
     {
         return $cells;
     }
@@ -97,7 +99,7 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
      *
      * @return array the modified control-icons
      */
-    public function makeControl($table, $row, $cells, &$parentObject)
+    public function makeControl($table, $row, $cells, &$parentObject): array
     {
         return $cells;
     }
@@ -112,7 +114,7 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
      *
      * @return array Array of modified cells/columns
      */
-    public function renderListHeader($table, $currentIdList, $headerColumns, &$parentObject)
+    public function renderListHeader($table, $currentIdList, $headerColumns, &$parentObject): array
     {
         return $headerColumns;
     }
@@ -127,7 +129,7 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
      *
      * @return array Array of modified clipboard/action icons
      */
-    public function renderListHeaderActions($table, $currentIdList, $cells, &$parentObject)
+    public function renderListHeaderActions($table, $currentIdList, $cells, &$parentObject): array
     {
         return $cells;
     }
@@ -141,7 +143,7 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
      * @param array $theData
      * @param DatabaseRecordListXclass $parentObj
      */
-    public function checkChildren($table, array $row, $level, array &$theData, DatabaseRecordListXclass $parentObj)
+    public function checkChildren(string $table, array $row, int $level, array &$theData, DatabaseRecordListXclass $parentObj)
     {
         if ($table === 'tt_content' && $row['CType'] === 'gridelements_pi1') {
             $elementChildren = Helper::getInstance()->getChildren(
@@ -175,9 +177,9 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
      */
     public function contentCollapseIcon(
         array &$data,
-        $sortField,
-        $level,
-        &$contentCollapseIcon,
+        string $sortField,
+        int $level,
+        string &$contentCollapseIcon,
         DatabaseRecordListXclass $parentObj
     ) {
         if ($data['_EXPAND_TABLE_'] === 'tt_content') {
@@ -205,7 +207,7 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
     /**
      * @return IconFactory
      */
-    public function getIconFactory()
+    public function getIconFactory(): IconFactory
     {
         if ($this->iconFactory === null) {
             $this->iconFactory = GeneralUtility::makeInstance(IconFactory::class);
@@ -219,7 +221,7 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
      *
      * @return LanguageService $languageService
      */
-    public function getLanguageService()
+    public function getLanguageService(): LanguageService
     {
         return $this->languageService;
     }
@@ -237,7 +239,7 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
     /**
      * @return BackendUserAuthentication
      */
-    public function getBackendUser()
+    public function getBackendUser(): BackendUserAuthentication
     {
         return $GLOBALS['BE_USER'];
     }

Classes/Backend/ItemsProcFuncs/AbstractItemsProcFunc.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GridElementsTeam\Gridelements\Backend\ItemsProcFuncs;
 
 /***************************************************************
@@ -23,6 +25,7 @@ namespace GridElementsTeam\Gridelements\Backend\ItemsProcFuncs;
 
 use GridElementsTeam\Gridelements\Helper\Helper;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\Database\QueryGenerator;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\SingletonInterface;
@@ -37,12 +40,12 @@ abstract class AbstractItemsProcFunc implements SingletonInterface
     /**
      * @var LanguageService
      */
-    protected $languageService;
+    protected LanguageService $languageService;
 
     /**
      * @var QueryGenerator
      */
-    protected $tree;
+    protected QueryGenerator $tree;
 
     /**
      * initializes this class
@@ -59,7 +62,7 @@ abstract class AbstractItemsProcFunc implements SingletonInterface
      *
      * @return array|null An array containing the data of the selected backend layout as well as a parsed version of the layout configuration
      */
-    public function getSelectedBackendLayout($pageId)
+    public function getSelectedBackendLayout(int $pageId): ?array
     {
         return Helper::getInstance()->getSelectedBackendLayout($pageId);
     }
@@ -71,7 +74,7 @@ abstract class AbstractItemsProcFunc implements SingletonInterface
      *
      * @return array
      */
-    public function getRootline($pageId)
+    public function getRootline(int $pageId): array
     {
         return BackendUtility::BEgetRootLine($pageId);
     }
@@ -81,7 +84,7 @@ abstract class AbstractItemsProcFunc implements SingletonInterface
      *
      * @return LanguageService $languageService
      */
-    public function getLanguageService()
+    public function getLanguageService(): LanguageService
     {
         return $this->languageService;
     }
@@ -99,9 +102,9 @@ abstract class AbstractItemsProcFunc implements SingletonInterface
     /**
      * Gets the current backend user.
      *
-     * @return \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
+     * @return BackendUserAuthentication
      */
-    public function getBackendUser()
+    public function getBackendUser(): BackendUserAuthentication
     {
         return $GLOBALS['BE_USER'];
     }

Classes/Backend/ItemsProcFuncs/CTypeList.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GridElementsTeam\Gridelements\Backend\ItemsProcFuncs;
 
 /***************************************************************
@@ -34,7 +36,7 @@ class CTypeList extends AbstractItemsProcFunc
     /**
      * @var LayoutSetup
      */
-    protected $layoutSetup;
+    protected LayoutSetup $layoutSetup;
 
     /**
      * ItemProcFunc for CType items
@@ -47,10 +49,10 @@ class CTypeList extends AbstractItemsProcFunc
             $colPos = is_array($params['row']['colPos']) ? $params['row']['colPos'][0] : $params['row']['colPos'];
             $this->checkForAllowedCTypes(
                 $params['items'],
-                $params['row']['pid'],
-                $colPos,
-                $params['row']['tx_gridelements_container'],
-                $params['row']['tx_gridelements_columns']
+                (int)$params['row']['pid'],
+                (int)$colPos,
+                (int)$params['row']['tx_gridelements_container'],
+                (int)$params['row']['tx_gridelements_columns']
             );
         } else {
             $this->init((int)$params['row']['pid']);
@@ -60,10 +62,10 @@ class CTypeList extends AbstractItemsProcFunc
             if ((int)$existingElement['pid'] > 0) {
                 $this->checkForAllowedCTypes(
                     $params['items'],
-                    $existingElement['pid'],
-                    $existingElement['colPos'],
-                    $existingElement['tx_gridelements_container'],
-                    $existingElement['tx_gridelements_columns']
+                    (int)$existingElement['pid'],
+                    (int)$existingElement['colPos'],
+                    (int)$existingElement['tx_gridelements_container'],
+                    (int)$existingElement['tx_gridelements_columns']
                 );
             }
         }
@@ -78,34 +80,27 @@ class CTypeList extends AbstractItemsProcFunc
      * @param int $gridContainerId The ID of the current container
      * @param int $gridColumn The grid column the element is a child of
      */
-    public function checkForAllowedCTypes(array &$items, $pageId, $pageColumn, $gridContainerId, $gridColumn)
+    public function checkForAllowedCTypes(array &$items, int $pageId, int $pageColumn, int $gridContainerId, int $gridColumn)
     {
-        $allowed = [];
-        $disallowed = [];
-        if ((int)$pageColumn >= 0 || (int)$pageColumn === -2) {
-            $column = $pageColumn ? $pageColumn : 0;
+        if ($pageColumn >= 0 || $pageColumn === -2) {
+            $column = $pageColumn ?: 0;
             $layout = $this->getSelectedBackendLayout($pageId);
         } else {
             $this->init($pageId);
-            $column = $gridColumn ? (int)$gridColumn : 0;
+            $column = $gridColumn ?: 0;
             $gridElement = $this->layoutSetup->cacheCurrentParent($gridContainerId, true);
-            $layout = $this->layoutSetup->getLayoutSetup($gridElement['tx_gridelements_backend_layout']);
+            $layout = $this->layoutSetup->getLayoutSetup($gridElement['tx_gridelements_backend_layout'] ?? '');
         }
         if (!empty($layout)) {
-            if (is_array($layout['allowed']) && is_array($layout['allowed'][$column]) && !empty($layout['allowed'][$column]['CType'])) {
-                $allowed = $layout['allowed'][$column]['CType'];
-            }
-            if (is_array($layout['disallowed']) && is_array($layout['disallowed'][$column]) && !empty($layout['disallowed'][$column]['CType'])) {
-                $disallowed = $layout['disallowed'][$column]['CType'];
-            }
-        }
-        if (isset($layout) && (!empty($allowed) || !empty($disallowed))) {
-            foreach ($items as $key => $item) {
-                if ((
-                    !empty($allowed) &&
+            $allowed = $layout['allowed'][$column]['CType'] ?? [];
+            $disallowed = $layout['disallowed'][$column]['CType'] ?? [];
+            if (!empty($allowed) || !empty($disallowed)) {
+                foreach ($items as $key => $item) {
+                    if ((
+                        !empty($allowed) &&
                         !isset($allowed['*']) &&
                         !isset($allowed[$item[1]])
-                ) ||
+                    ) ||
                     (
                         !empty($disallowed) &&
                         (
@@ -113,7 +108,8 @@ class CTypeList extends AbstractItemsProcFunc
                             isset($disallowed[$item[1]])
                         )
                     )) {
-                    unset($items[$key]);
+                        unset($items[$key]);
+                    }
                 }
             }
         }
@@ -124,12 +120,10 @@ class CTypeList extends AbstractItemsProcFunc
      *
      * @param int $pageId
      */
-    public function init($pageId = 0)
+    public function init(int $pageId = 0)
     {
         parent::init();
-        if (!$this->layoutSetup) {
-            $this->injectLayoutSetup(GeneralUtility::makeInstance(LayoutSetup::class)->init($pageId));
-        }
+        $this->injectLayoutSetup(GeneralUtility::makeInstance(LayoutSetup::class)->init($pageId));
     }
 
     /**

Classes/Backend/ItemsProcFuncs/ColPosList.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GridElementsTeam\Gridelements\Backend\ItemsProcFuncs;
 
 /***************************************************************
@@ -38,16 +40,22 @@ class ColPosList extends AbstractItemsProcFunc
     {
         $this->init();
         if ($params['row']['pid'] > 0) {
-            $contentType = is_array($params['row']['CType']) ? $params['row']['CType'][0] : $params['row']['CType'];
-            $listType = is_array($params['row']['list_type']) ? $params['row']['list_type'][0] : $params['row']['list_type'];
-            $gridType = is_array($params['row']['tx_gridelements_backend_layout']) ? $params['row']['tx_gridelements_backend_layout'][0] : $params['row']['tx_gridelements_backend_layout'];
+            if (isset($params['row']['CType'])) {
+                $contentType = is_array($params['row']['CType']) ? $params['row']['CType'][0] : $params['row']['CType'];
+            }
+            if (isset($params['row']['list_type'])) {
+                $listType = is_array($params['row']['list_type']) ? $params['row']['list_type'][0] : $params['row']['list_type'];
+            }
+            if (isset($params['row']['tx_gridelements_backend_layout'])) {
+                $gridType = is_array($params['row']['tx_gridelements_backend_layout']) ? $params['row']['tx_gridelements_backend_layout'][0] : $params['row']['tx_gridelements_backend_layout'];
+            }
             $params['items'] = $this->addColPosListLayoutItems(
                 $params['row']['pid'],
                 $params['items'],
-                $contentType,
-                $listType,
-                $gridType,
-                $params['row']['tx_gridelements_container']
+                $contentType ?? '',
+                $listType ?? '',
+                $gridType ?? '',
+                (int)($params['row']['tx_gridelements_container'] ?? 0)
             );
         } else {
             // negative uid_pid values indicate that the element has been inserted after an existing element
@@ -55,7 +63,7 @@ class ColPosList extends AbstractItemsProcFunc
             $existingElement = BackendUtility::getRecordWSOL(
                 'tt_content',
                 -((int)$params['row']['pid']),
-                'pid,CType,tx_gridelements_container'
+                'pid,CType,list_type,tx_gridelements_backend_layout,tx_gridelements_container'
             );
             if ($existingElement['pid'] > 0) {
                 $params['items'] = $this->addColPosListLayoutItems(
@@ -64,7 +72,7 @@ class ColPosList extends AbstractItemsProcFunc
                     $existingElement['CType'],
                     $existingElement['list_type'],
                     $existingElement['tx_gridelements_backend_layout'],
-                    $existingElement['tx_gridelements_container']
+                    (int)$existingElement['tx_gridelements_container']
                 );
             }
         }
@@ -83,13 +91,13 @@ class ColPosList extends AbstractItemsProcFunc
      * @return array $items The ready made array of items
      */
     protected function addColPosListLayoutItems(
-        $pageId,
+        int $pageId,
         array $items,
-        $contentType = '',
-        $listType = '',
-        $gridType = '',
-        $container = 0
-    ) {
+        string $contentType = '',
+        string $listType = '',
+        string $gridType = '',
+        int $container = 0
+    ): array {
         if (empty($container)) {
             $layout = $this->getSelectedBackendLayout($pageId);
             if ($layout) {

Classes/Backend/ItemsProcFuncs/ListTypeList.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GridElementsTeam\Gridelements\Backend\ItemsProcFuncs;
 
 /***************************************************************
@@ -34,7 +36,7 @@ class ListTypeList extends AbstractItemsProcFunc
     /**
      * @var LayoutSetup
      */
-    protected $layoutSetup;
+    protected LayoutSetup $layoutSetup;
 
     /**
      * ItemProcFunc for CType items
@@ -78,34 +80,27 @@ class ListTypeList extends AbstractItemsProcFunc
      * @param int $gridContainerId The ID of the current container
      * @param int $gridColumn The grid column the element is a child of
      */
-    public function checkForAllowedListTypes(array &$items, $pageId, $pageColumn, $gridContainerId, $gridColumn)
+    public function checkForAllowedListTypes(array &$items, int $pageId, int $pageColumn, int $gridContainerId, int $gridColumn)
     {
-        $allowed = [];
-        $disallowed = [];
-        if ((int)$pageColumn >= 0 || (int)$pageColumn === -2) {
-            $column = $pageColumn ? $pageColumn : 0;
+        if ($pageColumn >= 0 || $pageColumn === -2) {
+            $column = $pageColumn ?: 0;
             $layout = $this->getSelectedBackendLayout($pageId);
         } else {
             $this->init($pageId);
-            $column = $gridColumn ? (int)$gridColumn : 0;
+            $column = $gridColumn ?: 0;
             $gridElement = $this->layoutSetup->cacheCurrentParent($gridContainerId, true);
-            $layout = $this->layoutSetup->getLayoutSetup($gridElement['tx_gridelements_backend_layout']);
+            $layout = $this->layoutSetup->getLayoutSetup($gridElement['tx_gridelements_backend_layout'] ?? '');
         }
         if (!empty($layout)) {
-            if (is_array($layout['allowed']) && is_array($layout['allowed'][$column]) && !empty($layout['allowed'][$column]['list_type'])) {
-                $allowed = $layout['allowed'][$column]['list_type'];
-            }
-            if (is_array($layout['disallowed']) && is_array($layout['disallowed'][$column]) && !empty($layout['disallowed'][$column]['list_type'])) {
-                $disallowed = $layout['disallowed'][$column]['list_type'];
-            }
-        }
-        if (isset($layout) && (!empty($allowed) || !empty($disallowed))) {
-            foreach ($items as $key => $item) {
-                if ((
-                    !empty($allowed) &&
+            $allowed = $layout['allowed'][$column]['list_type'] ?? [];
+            $disallowed = $layout['disallowed'][$column]['list_type'] ?? [];
+            if (!empty($allowed) || !empty($disallowed)) {
+                foreach ($items as $key => $item) {
+                    if ((
+                        !empty($allowed) &&
                         !isset($allowed['*']) &&
                         !isset($allowed[$item[1]])
-                ) ||
+                    ) ||
                     (
                         !empty($disallowed) &&
                         (
@@ -113,7 +108,8 @@ class ListTypeList extends AbstractItemsProcFunc
                             isset($disallowed[$item[1]])
                         )
                     )) {
-                    unset($items[$key]);
+                        unset($items[$key]);
+                    }
                 }
             }
         }
@@ -124,12 +120,10 @@ class ListTypeList extends AbstractItemsProcFunc
      *
      * @param int $pageId
      */
-    public function init($pageId = 0)
+    public function init(int $pageId = 0)
     {
         parent::init();
-        if (!$this->layoutSetup) {
-            $this->injectLayoutSetup(GeneralUtility::makeInstance(LayoutSetup::class)->init($pageId));
-        }
+        $this->injectLayoutSetup(GeneralUtility::makeInstance(LayoutSetup::class)->init($pageId));
     }
 
     /**

Classes/Backend/ItemsProcFuncs/SysLanguageUidList.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GridElementsTeam\Gridelements\Backend\ItemsProcFuncs;
 
 /***************************************************************
@@ -47,7 +49,7 @@ class SysLanguageUidList extends AbstractItemsProcFunc
      * @param array $items The items of the current language list
      * @param int $gridContainerId The ID of the current container
      */
-    public function checkForAllowedLanguages(array &$items, $gridContainerId)
+    public function checkForAllowedLanguages(array &$items, int $gridContainerId)
     {
         if (!$gridContainerId) {
             return;

commit b4a9906e2a79b57832c5e0f54a0aad31449b7655

[BUGFIX] Fix regression introduced with e7e885ef
Closes #483


(cherry picked from commit 1c449306bdce8541d359058f29917f556fdd04c9)

Classes/Hooks/DatabaseRecordList.php
@@ -155,14 +155,16 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
                 0,
                 $parentObj->selectFields
             );
-            $layoutColumns = $parentObj->getGridelementsBackendLayouts()->getLayoutColumns((string)$row['tx_gridelements_backend_layout']);
-            if (!empty($elementChildren)) {
-                $theData['_CONTAINER_COLUMNS_'] = $layoutColumns;
-                $theData['_EXPANDABLE_'] = true;
-                $theData['_EXPAND_ID_'] = $table . ':' . $row['uid'];
-                $theData['_EXPAND_TABLE_'] = $table;
-                $theData['_LEVEL_'] = $level;
-                $theData['_CHILDREN_'] = $elementChildren;
+            if (!empty($row['tx_gridelements_backend_layout'])) {
+                $layoutColumns = $parentObj->getGridelementsBackendLayouts()->getLayoutColumns((string)$row['tx_gridelements_backend_layout']);
+                if (!empty($elementChildren)) {
+                    $theData['_CONTAINER_COLUMNS_'] = $layoutColumns;
+                    $theData['_EXPANDABLE_'] = true;
+                    $theData['_EXPAND_ID_'] = $table . ':' . $row['uid'];
+                    $theData['_EXPAND_TABLE_'] = $table;
+                    $theData['_LEVEL_'] = $level;
+                    $theData['_CHILDREN_'] = $elementChildren;
+                }
             }
         }
     }
@@ -187,7 +189,7 @@ class DatabaseRecordList implements RecordListHookInterface, RecordListGetTableH
             $expandTitle = htmlspecialchars($this->languageService->sL('LLL:EXT:gridelements/Resources/Private/Language/locallang_db.xlf:list.expandElement'));
             $collapseTitle = htmlspecialchars($this->languageService->sL('LLL:EXT:gridelements/Resources/Private/Language/locallang_db.xlf:list.collapseElement'));
             $expandedGridelements = $parentObj->getExpandedGridelements();
-            if ($expandedGridelements[$data['uid']]) {
+            if (!empty($expandedGridelements[$data['uid']])) {
                 $href = htmlspecialchars(($parentObj->listURL() . '&gridelementsExpand[' . (int)$data['uid'] . ']=0'));
                 $contentCollapseIcon = '<a class="btn btn-default t3js-toggle-gridelements-list open-gridelements-container" data-state="expanded" href="' . $href .
                     '" id="t3-gridelements-' . $data['uid'] . '" title="' . $collapseTitle

Classes/Plugin/Gridelements.php
@@ -575,8 +575,7 @@ class Gridelements extends ContentObjectRenderer
         array &$parentGridData,
         array &$parentRecordNumbers,
         array $typoScriptSetup = []
-    )
-    {
+    ) {
         $column_number = (int)($child['tx_gridelements_columns'] ?? 0);
         $columnKey = $column_number . '.';
         $columnSetupKey = isset($typoScriptSetup['columns.'][$columnKey]) ? $columnKey : 'default.';

Classes/Xclass/DatabaseRecordList.php
@@ -156,6 +156,7 @@ class DatabaseRecordList extends \TYPO3\CMS\Recordlist\RecordList\DatabaseRecord
         // Creating the list of fields to include in the SQL query
         $selectFields = $this->getFieldsToSelect($table, $this->fieldArray);
         if ($table === 'tt_content') {
+            $selectFields[] = 'tx_gridelements_backend_layout';
             $this->gridelementsBackendLayouts = GeneralUtility::makeInstance(LayoutSetup::class)->init($id);
         }
         $this->selectFields = implode(',', $selectFields);
@@ -782,7 +783,7 @@ class DatabaseRecordList extends \TYPO3\CMS\Recordlist\RecordList\DatabaseRecord
 
         if (!empty($theData['_EXPANDABLE_']) && $level < 8 && $row['l18n_parent'] == 0 && !empty($theData['_CHILDREN_'])) {
             $expanded = !empty($this->expandedGridelements[$row['uid']]) && (
-                (!empty($this->expandedGridelements[$row['tx_gridelements_container']]) && $expanded)
+                (!empty($row['tx_gridelements_container']) && !empty($this->expandedGridelements[$row['tx_gridelements_container']]) && $expanded)
                 || empty($row['tx_gridelements_container'])
             ) ? ' expanded' : '';
             $previousGridColumn = '';
@@ -870,7 +871,7 @@ class DatabaseRecordList extends \TYPO3\CMS\Recordlist\RecordList\DatabaseRecord
         $contentCollapseIcon = '';
 
         if (count($data) > 1) {
-            if (!empty($data['_EXPANDABLE_']) && $l10nParent) {
+            if (!empty($data['_EXPANDABLE_']) && !$l10nParent) {
                 $sortField = GeneralUtility::_GP('sortField') ? GeneralUtility::_GP('sortField') . ':' . (int)GeneralUtility::_GP('sortRev') : '';
                 /**
                  * @hook contentCollapseIcon