6c162a5874a93c1b7d1d4047a8f73b5bd0142b45
Michael Kröner <m.kroener@tripuls.de>
2023-11-09 13:49:29 +0100

    [BUGFIX] Add ext_emconf.php
    (cherry picked from commit 1d053e6d611e3c75849c37a855cb3d86ef9ef8a3)

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Grid Elements',
+    'description' => 'This extension extends the grid-based concept known from the backend layout of pages to regular content elements - the grid elements.
+It offers a lot of new features like advanced drag & drop or real references, that improve the usability of the page and list module to speed up the daily work with the backend.',
+    'category' => 'be',
+    'author' => 'Grid Elements Team',
+    'author_email' => 'info@cybercraft.de',
+    'author_company' => '',
+    'state' => 'alpha',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.0.0-12.4.99',
+        ],
+        'conflicts' => [
+            'container' => '',
+        ],
+        'suggests' => [
+        ],
+    ],
+];
